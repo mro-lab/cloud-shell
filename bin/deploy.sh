5ed045c06b9101d9e77e5c8466f62e7c383187cd
mro-lab <subscribe_address@jcom.home.ne.jp>
2022-11-11 10:51:02 +0900

    ・github アクセストークンの要求タイミングを変更 ・mrolab-shell の取得を変更 ・EOF 指定ミスを修正 ・awsctl 初期化を追加

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,56 +10,62 @@ BASH_HOME="${REAL_PATH%/*}"
 TOOL_HOME=$(realpath "${TOOL_HOME:-${BASH_HOME}/..}")
 
 #-------------------------------------
+# 環境初期化
+if ! [ -d "${HOME}/mrolab-shell" ]; then
+    git clone "https://github.com/mro-lab/mrolab-shell"
+fi 
+source ${HOME}/mrolab-shell/lib/argp.shm
+source ${HOME}/mrolab-shell/lib/git.shm
+
+# 引数解析
+argp "${@}"
+
+#-------------------------------------
 # root 設定
 function root_setup {
-	if ! type gcc; then
-		sudo su - << EOF
-		yum -y install openssl-devel
-		yum -y install bzip2-devel
-		yum -y install libffi-devel
-		yum -y install gcc 
-	fi
+    if ! type gcc; then
+        sudo su - <<- EOF
+        yum -y install openssl-devel
+        yum -y install bzip2-devel
+        yum -y install libffi-devel
+        yum -y install gcc 
 EOF
+    fi
 }
 
 # bashrc インストール
 function install_bashrc {
-	cp -f ${TOOL_HOME}/conf/.bashrc ${HOME}/.bashrc
+    cp -f ${TOOL_HOME}/conf/.bashrc ${HOME}/.bashrc
 }
 
 # python インストール
 function install_python {
-	local version="${1:-3.9.6}"
-	if ! [ -d "Python-${version}/python" ]; then
-		wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
-		gtar xvzf "Python-${version}.tgz"
-		(
-		    cd "Python-${version}"
-		    ./configure --enable-optimizations --prefix=${HOME}/python
-		    make install
-		)
-		ln -nfs python3 ${HOME}/python/bin/python
-		rm -rf ${HOME}/Python-${version}*
-	fi
+    local version="${1:-3.9.6}"
+    (
+        cd ${HOME}
+        wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "Python-${version}.tgz"
+        gtar xvzf "Python-${version}.tgz"
+        cd "Python-${version}"
+        ./configure --enable-optimizations --prefix=${HOME}/python
+        make install
+        ln -nfs python3 ${HOME}/python/bin/python
+    )
 }
 
-# git クローン
-function git_clone {
-	local git_path="${1:?ERROR: require git path. user/repository}"
-	local version="${2:+-d ${2}}"
-	local install="${3:-}"
-	if ! [ -d "${install}" ]; then
-	    git clone "https://$(github_access_token)@github.com/${git_path}" ${version} ${install}
-	fi
-}
-
-# github アクセストークン入力
-function github_access_token {
-    if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then
+# awsctl インストール
+function install_awsctl {
+    # github アクセストークン入力
+    export GITHUB_ACCESS_TOKEN
+    if [ -z "${GITHUB_ACCESS_TOKEN:-}" ]; then
         read -sp 'INFO: require github access token: ' GITHUB_ACCESS_TOKEN
         echo '' # 入力完了を示す echo
     fi
-    echo "${GITHUB_ACCESS_TOKEN}"
+    
+    python -m pip install boto3
+    python -m pip install pybase62
+    ln -nfs mrolab-python ${HOME}/mrolab
+    git_clone "mro-lab/mrolab-python" "1.2.mro" "${GITHUB_ACCESS_TOKEN}" ${HOME}/mrolab-python
+    git_clone "mro-lab/awsctl" "2.0.mro" "${GITHUB_ACCESS_TOKEN}" ${HOME}/awsctl
 }
 
 # 全部設定
@@ -67,10 +73,10 @@ function all {
     root_setup
     install_bashrc
     install_python
-    exit	# 強制的に exit して .bashrc を読み直す
+    install_awsctl
+    echo "please 'exit' once for .bashrc reload."
 }
 
-#-------------------------------------
 # 使い方
 function usage {
     cat ${0} | sed -n '/^#.*#$/,$p' | egrep ';;|#'
@@ -78,21 +84,12 @@ function usage {
 
 
 #-------------------------------------
-# 環境初期化
-git_clone "mro-lab/mrolab-shell" "" "${HOME}/mrolab-shell"
-source ${HOME}/mrolab-shell/lib/argp.shm
-source ${HOME}/mrolab-shell/lib/common.shm
-source ${HOME}/mrolab-shell/lib/config.shm
-
-# 引数解析
-argp "${@}"
-
 # action #
 case "${ARGP[0]:-}" in
-    root_setup)             root_setup			"${ARGP[@]:1}" ;;   # 
-    install_bashrc)			install_bashrc		"${ARGP[@]:1}" ;;   # 
-    install_python)			install_python		"${ARGP[@]:1}" ;;   # 
-    github_access_token)	github_access_token	"${ARGP[@]:1}" ;;   # 
+    root_setup)             root_setup          "${ARGP[@]:1}" ;;   # root 設定
+    install_bashrc)         install_bashrc      "${ARGP[@]:1}" ;;   # bashrc インストール
+    install_python)         install_python      "${ARGP[@]:1}" ;;   # python インストール
+    install_awsctl)         install_awsctl      "${ARGP[@]:1}" ;;   # awsctl インストール
     all)                    all                 "${ARGP[@]:1}" ;;   # 全部設定
     *)                      usage                              ;;   # 使い方
 esac
