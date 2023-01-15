af850468dfbfe16a988648c45ba09a08dab60587
mro-lab <subscribe_address@jcom.home.ne.jp>
2023-01-15 23:25:32 +0900

    ・mrolab-shell のバージョンを確定 ・awsctl リポジトリ系のインストールを廃止：手順レイヤで導入する方針に変更

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,7 +12,7 @@ TOOL_HOME=$(realpath "${TOOL_HOME:-${BASH_HOME}/..}")
 #-------------------------------------
 # 環境初期化
 if ! [ -d "${HOME}/mrolab-shell" ]; then
-    git clone "https://github.com/mro-lab/mrolab-shell" -b 2.2.mro
+    git clone "https://github.com/mro-lab/mrolab-shell" -b 2.2
 fi 
 source ${HOME}/mrolab-shell/bin/import
 
@@ -40,32 +40,21 @@ function install_bashrc {
 # python インストール
 function install_python {
     local version="${1:-3.9.6}"
-    
-    if ${HOME}/python/bin/python --version | grep "${version}"; then
-        echo "pytohn installed."
-        return 0
+    if ! ${HOME}/python/bin/python --version | grep "${version}"; then
+        (
+            cd ${HOME}
+            wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "Python-${version}.tgz"
+            gtar xvzf "Python-${version}.tgz"
+            cd "Python-${version}"
+            ./configure --enable-optimizations --prefix=${HOME}/python
+            make install
+            ln -nfs python3 ${HOME}/python/bin/python
+            rm -rf ${HOME}/Python-${version}        # 容量削減のため削除
+            rm -rf ${HOME}/Python-${version}.tgz    # 容量削減のため削除
+        )
     fi
-    (
-        cd ${HOME}
-        wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "Python-${version}.tgz"
-        gtar xvzf "Python-${version}.tgz"
-        cd "Python-${version}"
-        ./configure --enable-optimizations --prefix=${HOME}/python
-        make install
-        ln -nfs python3 ${HOME}/python/bin/python
-        rm -rf ${HOME}/Python-${version}		# 容量削減のため削除
-        rm -rf ${HOME}/Python-${version}.tgz	# 容量削減のため削除
-    )
-}
-
-# awsctl インストール
-function install_awsctl {
     ${HOME}/python/bin/python -m pip install boto3
     ${HOME}/python/bin/python -m pip install pybase62
-    source ${BASH_HOME}/github_credential
-    ln -nfs mrolab-python ${HOME}/mrolab
-    git_clone "${HOME}/mrolab-python" "mro-lab/mrolab-python" "1.2" "${GITHUB_CREDENTIAL}"
-    git_clone "${HOME}/awsctl" "mro-lab/awsctl" "2.0" "${GITHUB_CREDENTIAL}"
 }
 
 # 全部設定
@@ -73,7 +62,6 @@ function all {
     root_setup
     install_bashrc
     install_python
-    install_awsctl
     echo "please 'exit' once for .bashrc reload."
 }
 
@@ -89,7 +77,6 @@ case "${ARGP[0]:-}" in
     root_setup)             root_setup          "${ARGP[@]:1}" ;;   # root 設定
     install_bashrc)         install_bashrc      "${ARGP[@]:1}" ;;   # bashrc インストール
     install_python)         install_python      "${ARGP[@]:1}" ;;   # python インストール
-    install_awsctl)         install_awsctl      "${ARGP[@]:1}" ;;   # awsctl インストール
     all)                    all                 "${ARGP[@]:1}" ;;   # 全部設定
     *)                      usage                              ;;   # 使い方
 esac
